7b5ddf986d0e202a7bbc33344659bdcffb1c8413
Ppkk13579 <pirokkie.k@gmail.com>
2026-07-19 20:36:34 +0900

    Update spreadsheet.js

---
+++ js/spreadsheet.js
@@ -0,0 +1,17 @@
+console.log("spreadsheet.js 読み込みOK");
+
+const sheetURL =
+"https://docs.google.com/spreadsheets/d/1nv3A-VtvRjM_UiAIIt7NdgQxzUb88PtPM9MIuUX24hg/gviz/tq?tqx=out:json&sheet=matches";
+
+
+fetch(sheetURL)
+.then(res => res.text())
+.then(data => {
+
+    const json = JSON.parse(
+        data.substring(47).slice(0,-2)
+    );
+
+    console.log(json);
+
+});
