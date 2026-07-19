cb10d0dfec576934f1662d17e698588273ea5e78
Ppkk13579 <pirokkie.k@gmail.com>
2026-07-20 07:48:04 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,41 +1,44 @@
 const matchList = document.getElementById("matchList");
 
+window.addEventListener("matchesLoaded", () => {
 
-const today = new Date();
-today.setHours(0, 0, 0, 0);
+    const today = new Date();
+    today.setHours(0, 0, 0, 0);
 
-const upcomingMatches = matches.filter(match => {
+    const upcomingMatches = matches.filter(match => {
 
-    const matchDate = new Date(match.date + "T00:00:00");
+        const matchDate = new Date(match.date + "T00:00:00");
 
-    return matchDate >= today;
-
-});
-
-upcomingMatches.sort((a, b) =>
-    new Date(a.date) - new Date(b.date)
-);
-
-// 試合がない場合
-if (upcomingMatches.length === 0) {
-
-    matchList.innerHTML = "<p>今シーズンの試合は終了しました。</p>";
-
-} else {
-
-    const nextDate = upcomingMatches[0].date;
-
-    const todayMatches = upcomingMatches.filter(match =>
-        match.date === nextDate
-    );
-
-    todayMatches.forEach(match => {
-
-        matchList.innerHTML += createMatchCard(match);
+        return matchDate >= today;
 
     });
 
-}
+    upcomingMatches.sort((a, b) =>
+        new Date(a.date) - new Date(b.date)
+    );
+
+    // 試合がない場合
+    if (upcomingMatches.length === 0) {
+
+        matchList.innerHTML = "<p>今シーズンの試合は終了しました。</p>";
+
+    } else {
+
+        const nextDate = upcomingMatches[0].date;
+
+        const todayMatches = upcomingMatches.filter(match =>
+            match.date === nextDate
+        );
+
+        todayMatches.forEach(match => {
+
+            matchList.innerHTML += createMatchCard(match);
+
+        });
+
+    }
+
+});
 
 function createEventPreview(){
 
@@ -44,17 +47,13 @@ function createEventPreview(){
 
     if(!area) return;
 
-
     area.innerHTML = "";
 
-
     Object.values(eventSettings).forEach(event=>{
-
 
         if(!event.visible){
             return;
         }
-
 
         area.innerHTML += `
 
@@ -70,10 +69,8 @@ function createEventPreview(){
 
         `;
 
-
     });
 
 }
-
 
 createEventPreview();
